dd1247ba4598abab4676798ff92142ae15eee1a3
Joaquinabarca95 <joaquinabarca@live.com>
2021-08-23 19:48:03 +0000

    finished proyect

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(randomSuitGenerator());
+  document.querySelector(".number").innerHTML = randomNumberGenerator();
+
+  function randomNumberGenerator() {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let randomNumber = Math.floor(Math.random() * numbers.length);
+    return numbers[randomNumber];
+  }
+
+  function randomSuitGenerator() {
+    let suits = ["spade", "club", "diamond", "heart"];
+    let randomSuits = Math.floor(Math.random() * suits.length);
+    return suits[randomSuits];
+  }
 };
